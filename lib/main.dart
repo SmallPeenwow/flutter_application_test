400c761af4d24b614fe2fdef22a7fd79a6c364f1
SmallPeenwow <bowdencamryn@gmail.com>
2023-07-09 21:21:27 +0200

    5 App Bar Done

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: ,
+          title: Text('App Title'),
         ),
       ),
     );
